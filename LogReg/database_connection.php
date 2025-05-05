484dd53417edef095df2de6fd98dd22f9114e27a
Jhoe When <155896444+jowen-jowen@users.noreply.github.com>
2025-05-05 18:40:20 +0800

    Delete LogReg directory

--- LogReg/database_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = new mysqli("localhost", "root", "gCEk4bteN)QgwfbA", "db_android_login");
-
-    if ($conn->connect_error) {
-        echo json_encode(["status" => "error", "message" => "Database connection failed"]);
-        exit();
-    }
-?>
