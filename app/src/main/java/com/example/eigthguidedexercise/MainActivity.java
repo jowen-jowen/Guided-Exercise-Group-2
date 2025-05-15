d6e9fe09b4e0c09fe7d3a5d8d6c58adbf3c8c6fb
TEOPE, CEZAR DOMINIC T. <teopecezar6@gmail.com>
2025-05-15 15:04:02 +0800

    8th Guided Exercise Finished

--- app/src/main/java/com/example/eigthguidedexercise/MainActivity.java
+++ app/src/main/java/com/example/eigthguidedexercise/MainActivity.java
@@ -16,7 +16,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
